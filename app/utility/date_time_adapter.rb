2cbb16769d0f153dfc050072aefd11d1cba6b832
mikeyhogarth <mikehogarth20@hotmail.com>
2015-06-06 13:46:33 +0100

    Date parsing

--- app/utility/date_time_adapter.rb
+++ app/utility/date_time_adapter.rb
@@ -2,12 +2,14 @@
 # DateTimeAdapter
 # 
 class DateTimeAdapter
+  
+  FORMATTER = '%H%M %Y%m%d'
 
   #
   # self.convert
   #
   def self.convert(time_as_string, date_as_string)
-    return DateTime.now
+    return DateTime.strptime("#{time_as_string} #{date_as_string}", FORMATTER)
   end
 
 end
